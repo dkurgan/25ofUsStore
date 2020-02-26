d794f161c78266a4d4c5bcd95fae81ff0caa3491
Danila Kurgan <danilamound@gmail.com>
2020-02-25 22:09:45 -0800

    Updates

---
+++ precache-manifest.cb6a3a4883ccd1f3ecaa96510194de69.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65fc43cdf34b1a61d574f439998da709",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d3a2f1a750ad90ca7153",
+    "url": "/static/css/main.1f82c2b2.chunk.css"
+  },
+  {
+    "revision": "80603af050991b555ddb",
+    "url": "/static/js/2.185be988.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.185be988.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3a2f1a750ad90ca7153",
+    "url": "/static/js/main.7482dada.chunk.js"
+  },
+  {
+    "revision": "2ffd4c9e0deaef9e2661",
+    "url": "/static/js/runtime-main.be271b43.js"
+  },
+  {
+    "revision": "2747f083a2e092b3088be43b95e970aa",
+    "url": "/static/media/hoodie_1.2747f083.jpeg"
+  },
+  {
+    "revision": "eba0ea3556b015f52003b8c247ef4a02",
+    "url": "/static/media/hoodie_2.eba0ea35.jpeg"
+  },
+  {
+    "revision": "9fec3422f88296909bed8dfadf32f210",
+    "url": "/static/media/pants_1.9fec3422.jpeg"
+  }
+]);
